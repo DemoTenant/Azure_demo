26604f35545d1a7a0d340a26f8cd88551bf65098
Marina Volchek <marina.bespalov@microfocus.com>
2021-04-08 00:34:34 +0300

    refer to devweb_scripts

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 
 root_dir = os.getcwd()
-scripts_directory = os.path.join(root_dir, "scripts")
+scripts_directory = os.path.join(root_dir, "devweb-scripts")
 
 for dir in os.listdir(scripts_directory):
-    scriptDir = os.path.join("scripts", dir)
+    scriptDir = os.path.join("devweb-scripts", dir)
     if os.path.isdir(scriptDir) and not dir.startswith('.'):
                 os.system('%DEVWEB_PATH%\DevWeb.exe -mode=load ' + scriptDir)
